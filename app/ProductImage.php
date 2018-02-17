2a5679d54d1fd7cb53d4a1b4119e91481ca033af
Carmona <13030617@itcelaya.edu.mx>
2018-02-17 15:40:40 -0600

    Se crearon las relaciones hasMany y BelongsTo de Product,category, productImage, al corriente con clase 27

--- app/ProductImage.php
+++ app/ProductImage.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 use App\ProductImage;
 class ProductImage extends Model
 {
-    
+	//$productImage -> product
+
+    public function product(){
+
+    	return $this->belongsTo(Product::class);
+    }
 }
